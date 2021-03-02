e494d44084f49e0acc89ffab0c48355770a809e7
Parsa Nasirimehr <p.nasirimehr@gmail.com>
2021-03-02 10:09:30 +0330

    Initial Commit

---
+++ DiceeSwiftUI/DiceeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceeSwiftUIApp.swift
+//  DiceeSwiftUI
+//
+//  Created by Parsa Nasirimehr on 12/12/1399 AP.
+//
+
+import SwiftUI
+
+@main
+struct DiceeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
